5bad00cbbbbac304fca5d34e15a3d05624ea2a4a
aurora <dragoiaurora92@gmail.com>
2020-02-11 21:12:10 +0200

    Added problems with strings

--- VectoriMatrice/Program.cs
+++ VectoriMatrice/Program.cs
@@ -44,7 +44,7 @@ namespace vectori_matrice
         {
             int min, max, i;
             int[] vector2;
-            vector2 = new int[10]; 
+            vector2 = new int[10];
             vector2[0] = 1;
             vector2[1] = 2;
             vector2[2] = 3;
@@ -73,7 +73,7 @@ namespace vectori_matrice
         public static void TransformIntToDouble()
         {
             int i;
-            int[] vector; 
+            int[] vector;
             vector = new int[3];
             double[] vectorD;
             vectorD = new double[3];
@@ -82,47 +82,107 @@ namespace vectori_matrice
             vector[1] = 2;
             vector[2] = 3;
 
-            for (i=0; i<3; i++)
+            for (i = 0; i < 3; i++)
             {
                 vectorD[i] = (double)vector[i] + 1.5;
             }
-            for (i=0; i<3; i++)
+            for (i = 0; i < 3; i++)
             {
                 Console.WriteLine(vectorD[i]);
             }
-       
+
         }
         public static void Matrice()
         {
-           
+
             int[,] oMatrice = new int[,] { { 2, 7, 9 }, { 3, -6, 1 }, { 7, 4, 2 } };
             int i, j;
             // printing 2D array
-            for (i = 0; i < 3; i++)
+            for (i = 0; i <3; i++)
             {
                 for (j = 0; j < 3; j++)
                 {
-                    if (oMatrice[i, j] < 0)
-                        Console.WriteLine("Valoarea negativa are coordonatele: " + i +" , "+ j);
-                   // break;
+                    if(oMatrice[i, j] < 0)
+                        Console.WriteLine("Valoarea negativa are coordonatele: " + i + " , " + j);
+                    // break;
                 }
-                    //Console.Write(oMatrice[i,j] + " ");
+                //Console.Write(oMatrice[i,j] + " ");
                 //Console.WriteLine();
             }
-           
+        }
+        public static void PalindromeString()
+        {
+            String reversePalindrome = null;
+            String palindrome = "Dot saw I was Tod";
+            int len = palindrome.Length;
+            char[] tempCharArray = new char[len];
+            char[] charArray = new char[len];
+
+            // put original string in an
+            // array of chars
+            for (int i = 0; i < len; i++)
+            {
+                tempCharArray[i] = palindrome[i];
+
+                // reverse array of chars
+                for (int j = 0; j < len; j++)
+                {
+                    charArray[j] = tempCharArray[len - 1 - j];
+                }
+
+                reversePalindrome = new String(charArray);
+
+            }
+            Console.WriteLine(reversePalindrome);
         }
 
+        public static void TransformStringToArray()
+        {
+            String characters = "Build a new characters string";
+            int len = characters.Length;
+            int i;
+            char[] charArray = new char[len];
+            for (i = 0; i < len; i++)
+            {
+                charArray[i] = characters[i];
+            }
+            for (i = 0; i < len; i++)
+            {
+                Console.Write(charArray[i]);
+            }
+        }
+        public static void TransformEvenLetterUppercase()
+        {
+            String characters = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
+            int len = characters.Length;
+            int i;
+            char[] charArray = new char[len];
+            for (i = 0; i < len; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    //charArray[i] = characters[i].ToUpper();
+                    charArray[i] = Char.ToUpper(characters[i]);
+                }
+                else
+                    charArray[i] = characters[i];
+            }
+            for (i = 0; i < len; i++)
+            {
+                Console.Write(charArray[i]);
+            }
+        }
         static void Main(string[] args)
         {
             Vectori();
             AfisareElementeVectori();
             Matrice();
             DoubleVector();
-            MinMax();
-            Matrice();
+            MinMax();          
             TransformIntToDouble();
+            PalindromeString();
+            TransformStringToArray();
+            TransformEvenLetterUppercase();
         }
     }
-
-
 }
